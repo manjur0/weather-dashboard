79d934a06218fbaeffea2a591751996ec2b3932e
manjur0 <sfmanjur21@gmail.com>
2024-09-12 19:53:52 +0600

    Temparature, time and location added in daynamically

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import Header from "./Components/Headers/Header";
 import WeatherBoard from "./Components/weather/WeatherBoard";
-
+import { WeatherProvider } from "./Provider";
 const App = () => {
   return (
-    <div className="grid place-items-center h-screen">
-      <Header />
-      <main>
-        <section>
-          <WeatherBoard />
-        </section>
-      </main>
-    </div>
+    <WeatherProvider>
+      <div className="grid place-items-center h-screen">
+        <Header />
+        <main>
+          <section>
+            <WeatherBoard />
+          </section>
+        </main>
+      </div>
+    </WeatherProvider>
   );
 };
 
--- src/Components/weather/WeatherHeadLine.jsx
+++ src/Components/weather/WeatherHeadLine.jsx
@@ -1,21 +1,29 @@
 import PinIcon from "../../assets/pin.svg";
 import CloudIcon from "../../assets/cloud.svg";
+import { useContext } from "react";
+import { WeatherContext } from "../../Context";
+import { getDateTimeFormat } from "../../utils/Date-utils";
 const WeatherHeadLine = () => {
+  const { weatherData } = useContext(WeatherContext);
+  const { climate, temperature, location, time } = weatherData;
   return (
     <div>
       <div className="max-md:flex items-center justify-between md:-mt-10">
         <img src={CloudIcon} alt="cloud" />
         <div className="max-md:flex items-center max-md:space-x-4">
           <h1 className="text-[60px] lg:text-[80px] xl:text-[100px] leading-none md:mb-4">
-            16°
+            {Math.round(temperature)}°
           </h1>
           <div className="flex items-center space-x-4 md:mb-4">
             <img src={PinIcon} alt="pin" />
-            <h2 className="text-2xl lg:text-[50px]">Dhaka</h2>
+            <h2 className="text-2xl lg:text-[50px]">{location}</h2>
           </div>
         </div>
       </div>
-      <p className="text-sm lg:text-lg">06:09 - Sunday, 9 Dec ‘23</p>
+      <p className="text-sm lg:text-lg">
+        {getDateTimeFormat(time, "time", false)} -
+        {getDateTimeFormat(time, "date", false)}
+      </p>
     </div>
   );
 };
